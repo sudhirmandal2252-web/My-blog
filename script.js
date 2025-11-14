b1b49d48fbb4d281b47fcd0e06a1ef53516d9fef
Nikhil <sudhirmandal2252@gmail.com>
2025-11-14 18:34:34 +0530

    Update player.js

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
-// Mobile smooth play/pause tap
-const video = document.getElementById("mainVideo");
+var player = fluidPlayer('video-id', {
+    layoutControls: {
+        controlBar: {
+            autoHideTimeout: 3,
+            animated: true,
+            autoHide: true
+        },
+        htmlOnPauseBlock: {
+            html: null,
+            height: null,
+            width: null
+        },
+        autoPlay: true,
+        mute: true,
+        allowTheatre: true,
+        playPauseAnimation: false,
+        playbackRateEnabled: false,
+        allowDownload: false,
+        playButtonShowing: false,
+        fillToContainer: false,
+        posterImage: ""
+    },
 
-video.addEventListener("click", () => {
-    if (video.paused) video.play();
-    else video.pause();
+    vastOptions: {
+        adList: [
+            {
+                roll: "preRoll",
+                vastTag: "https://plumprush.com/dSmpF.zydLG/Nxv-ZYGSUL/yeTmR9luBZBU_lwkNPtTtYD3/MiDbEFxsMUTrgUtoNljGcYw/M/TeEwxGOUQi",
+                adText: ""
+            }
+        ],
+        adCTAText: false,
+        adCTATextPosition: ""
+    }
 });
